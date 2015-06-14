960424af4dec89d5f3fa2b8ae05027046610fc9d
Prokopenko Dmitry Andreevich <prorochek@gmail.com>
2015-06-15 03:06:19 +0400

    [*] project structure

--- Hessian/Server/HessianServer.java
+++ Hessian/Server/HessianServer.java
@@ -2,7 +2,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
 
-public class HessianTest {
+public class HessianServer {
 
     public static void main(String[] args) throws Exception {
         Server server = new Server(6969);
